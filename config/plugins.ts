1c686f6b031aeb351d23ca87bb9fbbcaba75345e
mohammadShamimReza <mohammadshamimreza23393@gmail.com>
2024-05-01 17:30:43 +0600

    connect to database

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,22 +1,19 @@
-// module.exports = ({ env }) => ({
-//   // ...
-//   email: {
-//     config: {
-//       provider: "nodemailer",
-//       providerOptions: {
-//         host: env("SMTP_HOST"),
-//         port: env("SMTP_PORT"),
-//         auth: {
-//           user: env("SMTP_USERNAME"),
-//           pass: env("SMTP_PASSWORD"),
-//         },
-//         // ... any custom nodemailer options
-//       },
-//       settings: {
-//         defaultFrom: "@strapi.io",
-//         defaultReplyTo: "@strapi.io",
-//       },
-//     },
-//   },
-//   // ...
-// });
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-supabase",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET"),
+        directory: env("SUPABASE_DIRECTORY"),
+        options: {},
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
