f6041443319087a231df5576a51f76197d24a589
mohammadshamimreza <mohammadshamimreza23393@gmail.com>
2025-02-28 22:28:56 +0600

    install mailgun

--- config/plugins.ts
+++ config/plugins.ts
@@ -25,6 +25,20 @@ module.exports = ({ env }) => ({
         delete: {},
       },
     },
+    email: {
+      config: {
+        provider: "mailgun",
+        providerOptions: {
+          key: env("MAILGUN_API_KEY"), // Required
+          domain: env("MAILGUN_DOMAIN"), // Required
+          url: env("MAILGUN_URL", "https://api.mailgun.net"), //Optional. If domain region is Europe use 'https://api.eu.mailgun.net'
+        },
+        settings: {
+          defaultFrom: "myemail@protonmail.com",
+          defaultReplyTo: "myemail@protonmail.com",
+        },
+      },
+    },
   },
   // ...
 });
